9429d0ec11201504540bab5fa8230f848c681c07
angel <albarros1973@gmail.com>
2021-02-09 17:32:37 +0100

    Create main_page.js and imports and exports

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hello");
+(()=>{var e={653:e=>{const r={initialize:()=>{console.log("Hi")}};e.exports=r}},r={};function o(t){if(r[t])return r[t].exports;var n=r[t]={exports:{}};return e[t](n,n.exports,o),n.exports}o.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return o.d(r,{a:r}),r},o.d=(e,r)=>{for(var t in r)o.o(r,t)&&!o.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},o.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r),(()=>{"use strict";var e=o(653);o.n(e)().initialize(),console.log("Hello")})()})();
