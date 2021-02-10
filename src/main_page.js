56f05672441183cca5670eadcddc9dfa49046128
angel <albarros1973@gmail.com>
2021-02-10 08:39:26 +0100

    Add image and article to main_page.js

--- src/main_page.js
+++ src/main_page.js
@@ -1,17 +1,39 @@
-const mainPage = (() => {
+import Main_image from './assets/jay-wennington-N_Y88TWmGwA-unsplash.jpg'
 
+const mainPage = (() => {
   const initialize = () => {
-    this.body = document.querySelector('body');
+    this.container = document.querySelector('#content');
   };
 
   const addTitle = (title) => {
     const h1 = document.createElement('h1');
     h1.textContent = title;
-    this.body.appendChild(h1);
+    this.container.appendChild(h1);
+  };
+
+  const addImage = (image) => {
+    const myImage = new Image();
+    myImage.src = image;
+    this.container.appendChild(myImage);
+  };
+
+  const addArticle = (title, body, date) => {
+    const h4 = document.createElement('h4');
+    h4.textContent = title;
+    const p = document.createElement('p');
+    p.textContent = body;
+    const p2 = document.createElement('p');
+    p2.textContent = date;
+    this.container.appendChild(document.createElement('br'));
+    this.container.appendChild(h4);
+    this.container.appendChild(p);
+    this.container.appendChild(p2);
   };
 
   const run = () => {
     addTitle('My Little Tavern');
+    addImage(Main_image);
+    addArticle('Heavenly food', 'I made a reservation in advance for my girlfriend, to say about the food that it was really heaven, we had a very good time, the behavior of the staff was excellent, I will definitely recommend', 'Date of visit: February 2021');
   };
 
   return { initialize, run };
--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,12 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
